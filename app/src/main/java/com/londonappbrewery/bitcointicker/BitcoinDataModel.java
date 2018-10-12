49f5bfa8f6efa92e095d102b75fb114b12c1efda
John <fordistance@hotmail.com>
2018-10-12 09:59:14 +0800

    Assignment finished

---
+++ app/src/main/java/com/londonappbrewery/bitcointicker/BitcoinDataModel.java
@@ -0,0 +1,29 @@
+package com.londonappbrewery.bitcointicker;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BitcoinDataModel {
+
+    private String mPrice;
+
+    public static BitcoinDataModel fromJson(JSONObject jsonObject){
+
+        try {
+
+            BitcoinDataModel bitcoinData = new BitcoinDataModel();
+
+            bitcoinData.mPrice = jsonObject.getString("ask");
+
+            return bitcoinData;
+
+        }catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String getPrice(){
+        return mPrice;
+    }
+}
